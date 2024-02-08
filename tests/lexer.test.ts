9c9f024124b92a842627990118ce1b89fab7050e
Mouthless Stoat <nphuyanh@gmail.com>
2024-02-08 08:03:12 -0400

    Added test function to lexer

--- tests/lexer.test.ts
+++ tests/lexer.test.ts
@@ -1,125 +1,89 @@
-import { test, expect, describe } from "bun:test"
+import { test as t, expect, describe } from "bun:test"
 import { Token, TokenType, tokenize as tkn } from "../src/front/lexer"
 
 const tokenize = (input: string) => tkn(input, false)
 
-test("Basic", () => {
-    expect(tokenize("1 + 1")).toEqual([
-        new Token(TokenType.Number, 0, 0, "1"),
-        new Token(TokenType.Plus, 0, 2, "+"),
-        new Token(TokenType.Number, 0, 4, "1"),
-    ])
-})
+function test(name: string, input: string, output: Token[]) {
+    t(name, () => {
+        expect(tokenize(input)).toEqual(output)
+    })
+}
 
-test("Long Symbol", () => {
-    expect(tokenize("veryLongSymbolForNoReason")).toEqual([
-        new Token(TokenType.Symbol, 0, 0, "veryLongSymbolForNoReason"),
-    ])
-})
+test("Basic", "1 + 1", [
+    new Token(TokenType.Number, 0, 0, "1"),
+    new Token(TokenType.Plus, 0, 2, "+"),
+    new Token(TokenType.Number, 0, 4, "1"),
+])
 
-test("List", () => {
-    expect(tokenize("[1,2,3]")).toEqual([
-        new Token(TokenType.OpenBracket, 0, 0, "["),
-        new Token(TokenType.Number, 0, 1, "1"),
-        new Token(TokenType.Comma, 0, 2, ","),
-        new Token(TokenType.Number, 0, 3, "2"),
-        new Token(TokenType.Comma, 0, 4, ","),
-        new Token(TokenType.Number, 0, 5, "3"),
-        new Token(TokenType.CloseBracket, 0, 6, "]"),
-    ])
-})
+test("Long Symbol", "veryLongSymbolForNoReason", [new Token(TokenType.Symbol, 0, 0, "veryLongSymbolForNoReason")])
 
-test("Multi-line", () => {
-    expect(tokenize("hello\n12")).toEqual([
-        new Token(TokenType.Symbol, 0, 0, "hello"),
-        new Token(TokenType.Number, 1, 0, "12"),
-    ])
-})
+test("List", "[1,2,3]", [
+    new Token(TokenType.OpenBracket, 0, 0, "["),
+    new Token(TokenType.Number, 0, 1, "1"),
+    new Token(TokenType.Comma, 0, 2, ","),
+    new Token(TokenType.Number, 0, 3, "2"),
+    new Token(TokenType.Comma, 0, 4, ","),
+    new Token(TokenType.Number, 0, 5, "3"),
+    new Token(TokenType.CloseBracket, 0, 6, "]"),
+])
 
-test("Keyword", () => {
-    expect(tokenize("fn hello")).toEqual([
-        new Token(TokenType.Function, 0, 0, "fn"),
-        new Token(TokenType.Symbol, 0, 3, "hello"),
-    ])
-})
+test("Multi-line", "hello\n12", [new Token(TokenType.Symbol, 0, 0, "hello"), new Token(TokenType.Number, 1, 0, "12")])
 
-test("Number Method", () => {
-    expect(tokenize("1.hello")).toEqual([
-        new Token(TokenType.Number, 0, 0, "1"),
-        new Token(TokenType.Dot, 0, 1, "."),
-        new Token(TokenType.Symbol, 0, 2, "hello"),
-    ])
-})
+test("Keyword", "fn hello", [new Token(TokenType.Function, 0, 0, "fn"), new Token(TokenType.Symbol, 0, 3, "hello")])
+
+test("Number Method", "1.hello", [
+    new Token(TokenType.Number, 0, 0, "1"),
+    new Token(TokenType.Dot, 0, 1, "."),
+    new Token(TokenType.Symbol, 0, 2, "hello"),
+])
 
 describe("Number", () => {
-    test("Negative Number", () => {
-        expect(tokenize("-1")).toEqual([new Token(TokenType.Minus, 0, 0, "-"), new Token(TokenType.Number, 0, 1, "1")])
-    })
+    test("Negative Number", "-1", [new Token(TokenType.Minus, 0, 0, "-"), new Token(TokenType.Number, 0, 1, "1")])
 
-    test("Negative Number 2", () => {
-        expect(tokenize("-1-2")).toEqual([
-            new Token(TokenType.Minus, 0, 0, "-"),
-            new Token(TokenType.Number, 0, 1, "1"),
-            new Token(TokenType.Minus, 0, 2, "-"),
-            new Token(TokenType.Number, 0, 3, "2"),
-        ])
-    })
+    test("Negative Number 2", "-1-2", [
+        new Token(TokenType.Minus, 0, 0, "-"),
+        new Token(TokenType.Number, 0, 1, "1"),
+        new Token(TokenType.Minus, 0, 2, "-"),
+        new Token(TokenType.Number, 0, 3, "2"),
+    ])
 
-    test("Floating-point", () => {
-        expect(tokenize("10.13")).toEqual([new Token(TokenType.Number, 0, 0, "10.13")])
-    })
+    test("Floating-point", "10.13", [new Token(TokenType.Number, 0, 0, "10.13")])
 
-    test("Floating-point 2", () => {
-        expect(tokenize("10.11 12.1934")).toEqual([
-            new Token(TokenType.Number, 0, 0, "10.11"),
-            new Token(TokenType.Number, 0, 6, "12.1934"),
-        ])
-    })
+    test("Floating-point 2", "10.11 12.1934", [
+        new Token(TokenType.Number, 0, 0, "10.11"),
+        new Token(TokenType.Number, 0, 6, "12.1934"),
+    ])
 
-    test("Floating-point 3", () => {
-        expect(tokenize("10.11.12.1934")).toEqual([
-            new Token(TokenType.Number, 0, 0, "10.11"),
-            new Token(TokenType.Dot, 0, 5, "."),
-            new Token(TokenType.Number, 0, 6, "12.1934"),
-        ])
-    })
+    test("Floating-point 3", "10.11.12.1934", [
+        new Token(TokenType.Number, 0, 0, "10.11"),
+        new Token(TokenType.Dot, 0, 5, "."),
+        new Token(TokenType.Number, 0, 6, "12.1934"),
+    ])
 
-    test("Scientific Notation", () => {
-        expect(tokenize("1e10")).toEqual([new Token(TokenType.Number, 0, 0, "1e10")])
-    })
+    test("Scientific Notation", "1e10", [new Token(TokenType.Number, 0, 0, "1e10")])
 
-    test("Scientific Notation 2", () => {
-        expect(tokenize("4e-10")).toEqual([new Token(TokenType.Number, 0, 0, "4e-10")])
-    })
+    test("Scientific Notation 2", "4e-10", [new Token(TokenType.Number, 0, 0, "4e-10")])
 
-    test("Scientific Notation 3", () => {
-        expect(tokenize("4e1.4")).toEqual([
-            new Token(TokenType.Number, 0, 0, "4e1"),
-            new Token(TokenType.Dot, 0, 3, "."),
-            new Token(TokenType.Number, 0, 4, "4"),
-        ])
-    })
+    test("Scientific Notation 3", "4e1.4", [
+        new Token(TokenType.Number, 0, 0, "4e1"),
+        new Token(TokenType.Dot, 0, 3, "."),
+        new Token(TokenType.Number, 0, 4, "4"),
+    ])
 
-    test("Multiple Numbers", () => {
-        expect(tokenize("11 2 333 4")).toEqual([
-            new Token(TokenType.Number, 0, 0, "11"),
-            new Token(TokenType.Number, 0, 3, "2"),
-            new Token(TokenType.Number, 0, 5, "333"),
-            new Token(TokenType.Number, 0, 9, "4"),
-        ])
-    })
+    test("Multiple Numbers", "11 2 333 4", [
+        new Token(TokenType.Number, 0, 0, "11"),
+        new Token(TokenType.Number, 0, 3, "2"),
+        new Token(TokenType.Number, 0, 5, "333"),
+        new Token(TokenType.Number, 0, 9, "4"),
+    ])
 })
 
 describe("String", () => {
-    test("Basic", () => {
-        expect(tokenize('"Hello World"')).toEqual([new Token(TokenType.String, 0, 0, '"Hello World"')])
-    })
+    test("Basic", '"Hello World"', [new Token(TokenType.String, 0, 0, '"Hello World"')])
 
-    test("Escape", () => {
-        expect(tokenize('"\\t\\r\\n"')).toEqual([new Token(TokenType.String, 0, 0, '"\\t\\r\\n"')])
-    })
+    test("Escape", '"\\t\\r\\n"', [new Token(TokenType.String, 0, 0, '"\\t\\r\\n"')])
 
-    test("Special", () => {
-        expect(tokenize('"\t"')).toEqual([new Token(TokenType.String, 0, 0, '"\t"')])
-    })
+    test("Special", '"\t"', [new Token(TokenType.String, 0, 0, '"\t"')])
 })
+
+describe("Comment", () => {})
